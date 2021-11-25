ec70e7edb5543f4dda6058f1d3767858f84dbb76
Aashish <aashish@easyrewardz.com>
2021-11-25 23:12:39 +0530

    class to functional component modify

--- react/src/Routes/ChildRoute.js
+++ react/src/Routes/ChildRoute.js
@@ -18,25 +18,25 @@ import MasterLayout from '../MY_REACT/MasterLayout/MasterLayout';
 
 export const ChildRoute = (props) => {
 
-        const { match } = props.urls;
-        
-        return (
-            <MasterLayout>
-                <PrivateRoute path={`${match.url}/hellomessage`} exact component={HelloMessage} />
-                <PrivateRoute path={`${match.url}/home`} exact component={Home} />
-                <PrivateRoute path={`${match.url}/cardtype`} exact component={CheckCardType} />
-                <PrivateRoute path={`${match.url}/qrcode`} exact component={QRCodeDemo} />
-                <PrivateRoute path={`${match.url}/linechart`} exact component={LineChart} />
-                <PrivateRoute path={`${match.url}/carddetails`} exact component={CardDetails} />
-                <PrivateRoute path={`${match.url}/slider`} exact component={CustomizedSlider} />
-                <PrivateRoute path={`${match.url}/circular`} exact component={CircularTimer} />
-                <PrivateRoute path={`${match.url}/textinput`} exact component={TextInput} />
-                <PrivateRoute path={`${match.url}/buttons`} exact component={Buttons} />
-                <PrivateRoute path={`${match.url}/otpinputfield`} exact component={OtpInputField} />
-                <PrivateRoute path={`${match.url}/selectdrop`} exact component={Select_Options} />
-                <PrivateRoute path={`${match.url}/checkbx`} exact component={Check_Box} />
-                <PrivateRoute path={`${match.url}/reactrender`} exact component={React_Parent_Render} />
-                <PrivateRoute path={`${match.url}/pwahome`} exact component={PwaHome} />
-            </MasterLayout>
-        );
+    const { match } = props.urls;
+
+    return (
+        <MasterLayout>
+            <PrivateRoute path={`${match.url}/hellomessage`} exact component={HelloMessage} />
+            <PrivateRoute path={`${match.url}/home`} exact component={Home} />
+            <PrivateRoute path={`${match.url}/cardtype`} exact component={CheckCardType} />
+            <PrivateRoute path={`${match.url}/qrcode`} exact component={QRCodeDemo} />
+            <PrivateRoute path={`${match.url}/linechart`} exact component={LineChart} />
+            <PrivateRoute path={`${match.url}/carddetails`} exact component={CardDetails} />
+            <PrivateRoute path={`${match.url}/slider`} exact component={CustomizedSlider} />
+            <PrivateRoute path={`${match.url}/circular`} exact component={CircularTimer} />
+            <PrivateRoute path={`${match.url}/textinput`} exact component={TextInput} />
+            <PrivateRoute path={`${match.url}/buttons`} exact component={Buttons} />
+            <PrivateRoute path={`${match.url}/otpinputfield`} exact component={OtpInputField} />
+            <PrivateRoute path={`${match.url}/selectdrop`} exact component={Select_Options} />
+            <PrivateRoute path={`${match.url}/checkbx`} exact component={Check_Box} />
+            <PrivateRoute path={`${match.url}/reactrender`} exact component={React_Parent_Render} />
+            <PrivateRoute path={`${match.url}/pwahome`} exact component={PwaHome} />
+        </MasterLayout>
+    );
 }
