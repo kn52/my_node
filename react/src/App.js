08a9b80c4be7836a7296170a763157a945365179
Aashish <aashish@easyrewardz.com>
2022-01-26 15:18:19 +0530

    li list swag

--- react/src/App.js
+++ react/src/App.js
@@ -3,5 +3,5 @@ import AppDemoClassComponent from './AppDemoClassComponent';
 import { ParentRoute } from './Routes/ParentRoute';
 
 // export const App = () => <ParentRoute/>;
-export const App = () => <AppDemoClassComponent/>;
-// export const App = () => <AppDemoFunctionComponent/>;
+// export const App = () => <AppDemoClassComponent/>;
+export const App = () => <AppDemoFunctionComponent/>;
--- react/src/AppDemoFunctionComponent.js
+++ react/src/AppDemoFunctionComponent.js
@@ -1,38 +1,70 @@
 import React, { useEffect } from "react";
 import './AppDemo.scss';
 import { adddemoservices } from "./AppDemoServices";
-import { ClickAwayListener } from '@material-ui/core';
-import { AppDemoChildFunctionComponent } from "./AppDemoChildComponent";
+import $ from 'jquery';
 
 export default function AppDemoFunctionComponent() {
 
-    const [open, setOpen] = React.useState(false);
+    const [tab, setTab] = React.useState(1);
 
     useEffect(()=>{
         let msg = adddemoservices.getwelcome() + "function component";
         console.log("Welcome Message",msg);
     },[])
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+    const handlelist = (li_val) => {
+        if (li_val < 7 && li_val > 0) {
+            if (tab != li_val) {
+                setTab(li_val);
+            }
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+            var list = $("#ullist");
+
+            if (list.length > 0) {
+                var l1 = list[0].childNodes;
+
+                if (l1.length > 0) {
+                    var l2 = (tab - 1 > -1) ? l1[tab - 1].childNodes : [];
+                    var l3 = (li_val - 1 > -1) ? l1[li_val - 1].childNodes : [];
+
+                    if (l2.length > 0 && tab > 0) {
+                        var l4 = l2[0];
+                        l4.style.color = "blue";
+                    }
+                    if (l3.length > 0 && tab < 7) {
+                        var l5 = l3[0];
+                        l5.style.color = "red";
+                    }
+                }
+            }
+        }
+        else {
+            return;
+        }
+    }
 
     return (
         <>
-            {
-                open ?
-                    <ClickAwayListener onClickAway={() => handleClose()}>
-                        <AppDemoChildFunctionComponent/>
-                    </ClickAwayListener>
-                    : null
-            }
-            <div id="zxc" style={{width:"200px",border: "1px solid red"}} onClick={() => handleClickOpen()}>
-                Hello
-            </div>
+           <ul id="ullist">
+               <li>
+                   <div onClick={(e) => handlelist(1)}>A</div>
+                </li>
+               <li>
+                   <div onClick={(e) => handlelist(2)}>B</div>
+                </li>
+               <li>
+                   <div onClick={(e) => handlelist(3)}>C</div>
+                </li>
+               <li>
+                   <div onClick={(e) => handlelist(4)}>D</div>
+                </li>
+               <li>
+                   <div onClick={(e) => handlelist(5)}>E</div>
+                </li>
+               <li>
+                   <div onClick={(e) => handlelist(6)}>F</div>
+                </li>
+           </ul>
         </>
     );
 }
