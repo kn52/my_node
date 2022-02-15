a2bff7c9fbf285465f2cd415dc8ce2ddcaef2145
Aashish <aashish@easyrewardz.com>
2022-02-15 11:02:02 +0530

    modify checkbox with square css

--- react/src/AppDemoClassComponent.js
+++ react/src/AppDemoClassComponent.js
@@ -17,29 +17,9 @@ export default class AppDemoClassComponent extends React.Component{
     componentDidMount(){
         let msg = adddemoservices.getwelcome() + "class component";
         console.log("Welcome Message",msg);
-        $(document).on('click', function (e) {
-            var container = $("#child");
-            var bool = (container.is(e.target) && container.has(e.target).length === 0) ? true : false;
-            if (bool === true && e.target.id === "zxc") {
-                $("#child").css('display','block');
-            }else if (bool === true && e.target.id === "child"){
-                $("#child").css('display','none');
-            }
-        })
-    }
-
-    handleClickOpen =  async () => {
-        let displayStyle = $("#child").css('display');
-        if(displayStyle === "block"){
-            $("#child").css('display','none');
-        }
-        else if (displayStyle === "none"){
-            $("#child").css('display','block');
-        }
     }
 
     render(){
-        const { childList } = this.state;
         return(
             <>
             <div className="without_lael_container">
--- react/src/MY_REACT/Check_Box/Check_Box.js
+++ react/src/MY_REACT/Check_Box/Check_Box.js
@@ -19,12 +19,15 @@ class Check_Box extends React.Component{
         return (
             <>
                 <div className="main_div">
-                    {/* <div className="without_label_container">
+                    <div className="without_label_container">
                         <input type="checkbox" name="check_test?" className="che_ck" />
-                    </div> */}
+                    </div>
                     <div className="with_label_container">
                         <input type="checkbox" id="custom_check" name="check_test??" className="che_ck" checked={this.state.isChecked}/>
                         <label htmlFor="custom_check" className="checkbox_label_style"></label>
+                    </div>
+                    <div className="with_square">
+                        <input type="checkbox" name="check_test?" className="che_ck" />
                     </div>
                 </div>
                 <div>
