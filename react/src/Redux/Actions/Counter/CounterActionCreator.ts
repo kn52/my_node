f2d2b3360e4849cb44ba25ee683d826e65e9b76b
Aashish <aashish@easyrewardz.com>
2022-08-07 16:47:44 +0530

    modify creator

--- react/src/Redux/Actions/Counter/CounterActionCreator.ts
+++ react/src/Redux/Actions/Counter/CounterActionCreator.ts
@@ -1,13 +1,6 @@
 import * as actionTypes from "./CounterActions"
 
-const _createAction = (_actionType: any, _count: any, _hits: any) => {
-  const action: any = {
-    type: _actionType,
-    count: _count,
-    hits: _hits
-  }
-  return action;
-}
+const _createAction = (_actionType: any, _count: any, _hits: any) => ({ type: _actionType, count: _count, hits: _hits })
 
 export const incrementCount = (_count: any, _hits: any) => _createAction(actionTypes.INCREMENT, _count,_hits)
 
--- react/src/_Demo/Demo.js
+++ react/src/_Demo/Demo.js
@@ -1,12 +1,11 @@
 import css_styles from '../_module/scss/_.module.scss'
 import var_styles from '../_module/scss/_var.module.scss'
 import React, { useRef, useState } from 'react';
-import { Article } from '../ts_component/Article/Article';
 
 export default function Demo() {
   return (
     <>
-      <Article />
+      <h1 style={{color:'#33b5e5'}}>Welcome to node!</h1>
     </>
   )
 }
