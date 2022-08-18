380f4e2df33694cf48ec3e0401fb82846074acf1
Aashish <aashish@easyrewardz.com>
2022-08-18 13:11:33 +0530

    modify mobx

--- react/src/_Demo/Demo.js
+++ react/src/_Demo/Demo.js
@@ -2,19 +2,42 @@ import css_styles from '../_module/scss/_.module.scss'
 import var_styles from '../_module/scss/_var.module.scss'
 import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
-export default function Demo({name, hi}) {
-  console.log("css_styles.theme_color",css_styles)
+import { makeAutoObservable } from "mobx"
+import { observer } from "mobx-react-lite"
+
+class Timer {
+  secondsPassed = 0
+
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  increaseTimer() {
+    this.secondsPassed += 1
+  }
+
+  decreaseTimer() {
+    this.secondsPassed -= 1
+  }
+
+  resetTimer() {
+    this.secondsPassed = 0
+  }
+}
+
+
+const Demo = observer(() => {
+  const [timer] = useState(() => new Timer()) // See the Timer definition above.
   return (
     <>
-      <h1 className={css_styles.them_color}>{hi} {name}</h1>
-      {/* <h1 style={{
-        color: var_styles.theme_color
-      }}>Welcome to node!</h1> */}
+      <span>Seconds passed: {timer.secondsPassed}</span>
+      <br />
+      <button onClick={() => timer.increaseTimer()}>increaseTimer</button>
+      <button onClick={() => timer.decreaseTimer()}>decreaseTimer</button>
+      <button onClick={() => timer.resetTimer()}>resetTimer</button>
     </>
   )
 }
+)
 
-Demo.propTypes = {
-  name: PropTypes.string,
-  hi: PropTypes.string
-}
+export default Demo
